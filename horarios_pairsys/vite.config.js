d58b1556056764356f3071607735bc442fa0cb2b
iiTzDeez <goncalomfvn@gmail.com>
2025-09-29 01:42:02 +0100

    .

--- horarios_pairsys/vite.config.js
+++ horarios_pairsys/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/horarios_pairsys/",  // 👈 nome exato do repo
 })
